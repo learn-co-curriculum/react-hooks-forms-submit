48589bc54bbd047787454d883d35b42e0dcfa317
Emmanuel Kipsang <emmqnuelkipsqng@gmail.com>
2023-04-23 09:31:14 -0700

    rendered error messages

--- src/components/Form.js
+++ src/components/Form.js
@@ -12,15 +12,25 @@ function Form(props) {
   function handleLastNameChange(event) {
     setLastName(event.target.value);
   }
+ // add state for holding ero messages
+  const [errors, setErrors] = useState([]);
 
   function handleSubmit (event) {
     event.preventDefault();
-    const formData = {firstName: firstName, lastName: lastName};
-    const dataArray = [...submittedData, formData];
-    setSubmittedData(dataArray);
-    setFirstName("");
-    setLastName("");
-  }
+    //first name is required
+    if(firstName.length > 0) {
+      const formData = {firstName: firstName, lastName: lastName};
+      const dataArray = [...submittedData, formData];
+      setSubmittedData(dataArray);
+      setFirstName("");
+      setLastName("");
+      setErrors([]);
+    }else {
+      setErrors(["First name is required!"])
+    }
+
+    }
+ 
   const listOfSubmissions = submittedData.map((data, index) => {
     return (
       <div key={index}>
@@ -36,6 +46,15 @@ function Form(props) {
         <input type="text" onChange={handleLastNameChange} value={lastName} />
         <button type="submit">Submit</button>
       </form>
+
+        {/* conditionally render error messages*/}
+      {/* conditionally render error messages*/}
+      {errors.length > 0 ? errors.map((error, index) => (
+        <p key={index} style={{color: "red"}}>
+          {error}
+        </p>
+      )) : null}
+
       <h3>Submissions</h3>
       {listOfSubmissions}
     </>
